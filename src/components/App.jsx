0e3eb580c1bbb0e073ba80fdda665d87c90ef464
neethunsr <neethu2001nsr@gmail.com>
2021-10-31 19:35:39 +0530

    Minor updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,38 @@
+import React, { useState, useEffect } from "react";
 import "../App.css";
+import axios from "axios";
 import CourseCard from "./CourseCard/CourseCard";
 import Header from "./Header/Header";
 
+const baseURL = "https://nut-case.s3.amazonaws.com/coursessc.json";
+
 function App() {
+	const [list, setList] = useState(null);
+
+	useEffect(() => {
+		axios.get(baseURL).then((response) => {
+			setList(response.data);
+		});
+	}, []);
+	if (!list) return null;
+
 	return (
 		<div className="App">
 			<Header />
 			<div className="courseCard">
-				<CourseCard />
-				<CourseCard />
-				<CourseCard />
-				<CourseCard />
-				<CourseCard />
-				<CourseCard />
-				<CourseCard />
-				<CourseCard />
-				<CourseCard />
-				<CourseCard />
-				<CourseCard />
-				<CourseCard />
+				{list.map((item, index) => (
+					<CourseCard
+						key={index}
+						id={item["Course Id"]}
+						date={item["Next Session Date"]}
+						name={item["Course Name"]}
+						provider={item["Provider"]}
+						issuer={item["Universities/Institutions"]}
+						parent={item["Parent Subject"]}
+						child={item["Child Subject"]}
+						url={item.Url}
+					/>
+				))}
 			</div>
 		</div>
 	);
--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -3,39 +3,39 @@ import "./CourseCard.css";
 import { CalendarToday } from "@mui/icons-material";
 
 function CourseCard(props) {
-	// const { name, imgsrc, rs, dis } = props;
+	// const { courseNum, courseDate, courseProvider } = props;
+	// console.log(props);
+	// console.log(courseNum);
 	return (
 		<div className="card">
 			<div className="cardTitle">
-				<h4>301</h4>
+				<h4>{props.id}</h4>
 				<p>
 					<CalendarToday color="action" />
-					Oct 11
+					{props.date}
 				</p>
 			</div>
 			<div className="courseDetails">
 				<div>
 					<h4 className="subHeading">Provider</h4>
-					<h3 className="provider">Udacity</h3>
+					<h3 className="provider">{props.provider}</h3>
 				</div>
 				<div>
 					<h4 className="subHeading">Course Name</h4>
-					<h3 className="courseName">
-						Introduction to Artificial Intelligence
-					</h3>
+					<h3 className="courseName">{props.name} </h3>
 				</div>
 				<div>
 					<h4 className="subHeading">Universities/Institutions</h4>
-					<h3 className="courseIssuer">Stanford University</h3>
+					<h3 className="courseIssuer">{props.issuer}</h3>
 				</div>
 				<div className="cardFooter">
 					<div>
 						<h4 className="subHeading">Parent Subject</h4>
-						<h3 className="courseSubject">Computer Science</h3>
+						<h3 className="courseSubject">{props.parent}</h3>
 					</div>
 					<div>
 						<h4 className="subHeading">Child Subject</h4>
-						<h3 className="courseSubject">AI</h3>
+						<h3 className="courseSubject">{props.child}</h3>
 					</div>
 				</div>
 			</div>
