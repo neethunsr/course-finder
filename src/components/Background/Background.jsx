fd3aed1a5b0a415df9d2318f47b5b5a6ca0b2e83
neethunsr <neethu2001nsr@gmail.com>
2021-11-16 17:02:59 +0530

    Changed folder structure

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Background.css";
+
+export default function Background() {
+	return (
+		<div className="">
+			<div className="circle"></div>
+			<div className="triangle"></div>
+			<div className="square"></div>
+			<div className="circle1"></div>
+			{/* <div className="triangle1"></div> */}
+		</div>
+	);
+}
